994e8efe0059d8e3dac60bdd993235f8563a1f65
Niaz Babar Bahadur <babar.bahadur@hotmail.com>
2024-12-25 22:33:25 +0100

    Changes reverted

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -46,7 +46,7 @@ const About = () => {
               transition={{ duration: 0.8, ease: [0.25, 0.1, 0.25, 1] }}
             >
               <Image
-                src="/babarr.png"
+                src="/babar.png"
                 alt="babar"
                 className="rounded-md"
                 width={600}
